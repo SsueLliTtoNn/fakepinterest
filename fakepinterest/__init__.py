d5b0ef2f718c3ba5f7b96cacf22d9a836dc4696b
José Sueliton <zezinho_sueliton@hotmail.com>
2024-07-31 16:10:41 -0300

    modificando base de dados

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -2,10 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
-import os
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 app.config["SECRET_KEY"] = "1fa8c19f305b3325f42756b500532c8b"
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
